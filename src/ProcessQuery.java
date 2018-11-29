c99b5b3fd4aa672f11ceba266f01be52a4a0f633
medicily@hotmail.com <yulei92@tamu.edu>
2018-11-29 10:54:13 -0600

    Add drop; need to implement select

--- src/ProcessQuery.java
+++ src/ProcessQuery.java
@@ -29,17 +29,45 @@ public class ProcessQuery {
         parser.commandSplit(query);
         System.out.print("Keyword: " + parser.keyword+"\n");
             System.out.print("TableList: " + parser.table_List+"\n");
-        if(parser.keyword.get(0).equals("create")){
-            op_create();
-        }else if(parser.keyword.get(0).equals("drop")){
-            op_drop();
-        }else if(parser.keyword.get(0).equals("insert")){
-            op_insert();
+        switch (parser.keyword.get(0)){
+            case "create" : op_create();
+                            break;
+            case "drop"   : op_drop();
+                            break;
+            case "insert" : op_insert();
+                            break;
+            case "delete" : op_delete();
+                            break;
+            case "select" : op_select();
+                            break;
+
+            default: System.out.println("Nothing to do.\n\n");
+        }
+    }
+
+    //进度：仅仅实现单个table的select
+    private void op_select(){
+
+    }
+
+    private void op_delete(){
+        System.out.print("\nStart to delete.\n");
+        if(parser.delete.where_clause_string == null) { //实现不包含where的情况
+            String relation_name = parser.table_List.get(0);
+            schema_manager.deleteRelation(relation_name);
+
+            System.out.print("\nTable " + relation_name + " has been deleted.\n");
+
+            //The following print is just the test.
+            System.out.print("\nCurrent schemas and relations: " + "\n");
+            System.out.print(schema_manager + "\n");
+            System.out.print("From the schema manager, the table " + relation_name + " exists: "
+                    + (schema_manager.relationExists(relation_name) ? "TRUE" : "FALSE") + "\n");
         }
     }
 
     //realize insert without select
-    //Insert需要完成的： 1. 对block方面进行优化，不能随意采用制定的block 2.集成select语句
+    //Insert需要完成的： 1.集成select语句
     private void op_insert(){
         System.out.print("\nStart to insert.\n");
         String relation_name = parser.table_List.get(0);
@@ -102,6 +130,9 @@ public class ProcessQuery {
         Schema schema=new Schema(field_names,field_types);
         Relation relation_reference=schema_manager.createRelation(relation_name,schema);
 
+        System.out.print("The schema has " + schema.getNumOfFields() + " fields" + "\n");
+        System.out.print("The schema allows " + schema.getTuplesPerBlock() + " tuples per block" + "\n");
+        
         System.out.print("\nThe created table has name " + relation_reference.getRelationName() + "\n");
         System.out.print("The table has schema:" + "\n");
         System.out.print(relation_reference.getSchema() + "\n");
@@ -135,18 +166,18 @@ public class ProcessQuery {
             relation_reference.getBlock(relation_reference.getNumOfBlocks()-1,memory_block_index);
             block_reference=mem.getBlock(memory_block_index);
 
-        if (block_reference.isFull()) {
-            System.out.print("(The block is full: Clear the memory block and append the tuple)" + "\n");
-            block_reference.clear(); //clear the block
-            block_reference.appendTuple(tuple); // append the tuple
-            System.out.print("Write to a new block at the end of the relation" + "\n");
-            relation_reference.setBlock(relation_reference.getNumOfBlocks(),memory_block_index); //write back to the relation
-        } else {
-            System.out.print("(The block is not full: Append it directly)" + "\n");
-            block_reference.appendTuple(tuple); // append the tuple
-            System.out.print("Write to the last block of the relation" + "\n");
-            relation_reference.setBlock(relation_reference.getNumOfBlocks()-1,memory_block_index); //write back to the relation
+            if (block_reference.isFull()) {
+                System.out.print("(The block is full: Clear the memory block and append the tuple)" + "\n");
+                block_reference.clear(); //clear the block
+                block_reference.appendTuple(tuple); // append the tuple
+                System.out.print("Write to a new block at the end of the relation" + "\n");
+                relation_reference.setBlock(relation_reference.getNumOfBlocks(),memory_block_index); //write back to the relation
+            } else {
+                System.out.print("(The block is not full: Append it directly)" + "\n");
+                block_reference.appendTuple(tuple); // append the tuple
+                System.out.print("Write to the last block of the relation" + "\n");
+                relation_reference.setBlock(relation_reference.getNumOfBlocks()-1,memory_block_index); //write back to the relation
+            }
         }
     }
-  }    
 }
